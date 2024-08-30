02a09fc34a1c8c389be565e737e53a30a9e009ae
1shubh-kumar65 <162529083+1shubh-kumar65@users.noreply.github.com>
2024-08-30 13:16:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def expensive_computation():
         # Imagine a time-consuming computation here
         
         questions = [
-            'Which yoga asana depicts an acute angle ?',
-                'which state has the highest health index in india'
+            'Which Yoga Asana depicts an Acute angle ?',
+                'Which state has the highest Health Index in India ?',
+                'Who is the famous Indian Women Mathematician popularly know as "Human Calculator"?'
         ]
         # question = ''
         
-        choice = 1
+        choice = 2
         
         question = questions[choice]
         
@@ -31,7 +32,7 @@ question = expensive_computation()
 a = st.chat_input('enter your answer here')
 
 if a:
-    persauna = 'you are an ai which check the answer given by the user the question will be provided to you and you have to tell if it is carrect or incorrect and why and what will be the coeerct answer if incorrect and also mention the question some data is kerela has the highest health index in india the question is :'
+    persauna = 'you are an ai which check the answer given by the user the question will be provided to you and you have to tell if it is carrect or incorrect and why and what will be the coeerct answer if incorrect and also mention the question some data is kerela has the highest health index in india, sakutala devi is the human calculator the question is :'
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(persauna+question+'the answer is :'+a)
     st.write(response.text)
